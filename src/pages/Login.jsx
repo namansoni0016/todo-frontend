ad86f885eff3d17c2e01cee0e843c61aeb1e94e7
Naman Soni <namantsoni@gmail.com>
2024-03-27 11:54:36 +0530

    Creating Login and Register Pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>Login</div>
-    )
-}
+        <div className='login'>
+            <section>
+                <form>
+                    <input type="email" placeholder='Email'/>
+                    <input type="password" placeholder='Password' />
+                    <button type="submit">Login</button>
+                    <h4>Or</h4>
+                    <Link to="/register">Sign Up</Link>
+                </form>
+            </section>
+        </div>
+    );
+};
 
 export default Login
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <div className='login'>
+            <section>
+                <form>
+                    <input type="name" placeholder='Name'/>
+                    <input type="email" placeholder='Email'/>
+                    <input type="password" placeholder='Password' />
+                    <button type="submit">Sign Up</button>
+                    <h4>Or</h4>
+                    <Link to="/login">Login</Link>
+                </form>
+            </section>
+        </div>
     )
 }
 
